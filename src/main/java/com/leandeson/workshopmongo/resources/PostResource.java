3055acd742728e1e8ce88e966960f71132d9fed4
Leandeson Pinheiro Santos de Araújo <leandersonpinheiro20@gmail.com>
2024-03-12 16:26:05 -0300

    Consulta simples com @Query e Query methods

--- src/main/java/com/leandeson/workshopmongo/resources/PostResource.java
+++ src/main/java/com/leandeson/workshopmongo/resources/PostResource.java
@@ -3,6 +3,7 @@ package com.leandeson.workshopmongo.resources;
 import com.leandeson.workshopmongo.domain.Post;
 import com.leandeson.workshopmongo.domain.User;
 import com.leandeson.workshopmongo.dto.UserDTO;
+import com.leandeson.workshopmongo.resources.util.URL;
 import com.leandeson.workshopmongo.services.PostService;
 import com.leandeson.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -27,6 +29,15 @@ public class PostResource {
         Post obj = postService.findById(id);
 
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) throws UnsupportedEncodingException {
+        text = URL.decodeParam(text);
+        List<Post> list = postService.findByTitle(text);
+
+
+        return ResponseEntity.ok().body(list);
     }
 
 
